e779cdd08eedf693a3c71062c75f2906648d0679
wangchao <3523235371@qq.com>
2025-07-23 23:04:09 +0800

    day1:面向对象

---
+++ JavaSE/src/main/java/object/one/Main.java
@@ -0,0 +1,15 @@
+package object.one;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Number number = new Number();
+        System.out.println(number.getAge());
+        System.out.println(number.getName());
+        System.out.println(number.getSex());
+        System.out.println(number.getIsFlag());
+        System.out.println(number.isFlag());
+        System.out.println(number.getIsDel());
+    }
+}
---
+++ JavaSE/src/main/java/object/one/Number.java
@@ -0,0 +1,14 @@
+package object.one;
+
+import lombok.Data;
+
+@Data
+public class Number {
+    private int age;
+    private char sex;
+    private boolean flag;
+    private Character isDel;
+    private String name;
+    private Boolean isFlag;
+
+}
